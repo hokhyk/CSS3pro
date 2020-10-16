29f69e95fc330d72361c1973f3470218fde379ee
hok <hokhyk@aliyun.com>
2020-10-16 17:18:08 +0800

    background gradient.

--- src/app/gradient-background/gradient-background.component.ts
+++ src/app/gradient-background/gradient-background.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gradient-background',
   templateUrl: './gradient-background.component.html',
-  styleUrls: ['./gradient-background.component.sass']
+  styleUrls: ['./gradient-background.component.scss']
 })
 export class GradientBackgroundComponent implements OnInit {
 
